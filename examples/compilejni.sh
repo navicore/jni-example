38385777c648f314e0c2255e452bf53109346538
Ed Sweeney <ed@onextent.com>
2018-06-02 06:22:43 -0700

    cleanup

--- examples/compilejni.sh
+++
@@ -1,4 +0,0 @@
-scalac src/main/scala/onextent/jniexample/Sample1.scala
-LIBS_HOME=/usr/local/Cellar/scala/2.12.6/libexec/lib
-CP=$LIBS_HOME/scala-library.jar
-javah -cp $CP:. onextent.jniexample.Sample1
